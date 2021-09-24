ec6ec68092cffba1226af2f75d0374ce0e938b84
Ian <ianmcm.89@gmail.com>
2021-09-24 10:05:30 -0400

    commit tree 10f83ae0c8ad23827f2855a79743ea37b97c0dda

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  //Find if target is one of the elements in the array.
+  if (array.find(element => element === target)) {
+    //console.log('Target is in array.');
+    return true;
+  } else {
+    for (let integer of array) {
+      //Subtract the integer from the target.
+      let search = target - array[integer];
+      //console.log(search)
+      //Filter through array, returning all numbers except the current integer.
+      const filtered = array.filter(num => num !== array[integer]);
+      if (filtered.find(element => element === search)) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 /* 
@@ -7,16 +23,43 @@ function hasTargetSum(array, target) {
 */
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+
+  Find if target is one of the elements in the array:
+    If not:
+  Iterate through the array:
+    Subtract the integer from the target.
+    Filter through array, returning all numbers except the current integer.
+    Find an integer that matches the subtracted value of the integer and target.
+      Return true.
+    if not:
+      Return false. 
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+
+  1) First use the find() method to search through the array, looking for an element that matches the target. If yes return true.
+
+  2) If no target match is found, iterate through the array:
+      On each iteration, subtract the integer from the target and assign it to a value.
+      Filter through the array, returning all numbers in the array that do not equal the current integer.
+      Use the find() method to locate an element in the filtered array that equals the subtracted value.
+
+  3) If a match is found, the algorithm returns true and if no matches are found the algorithm returns false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  // add your own custom tests in here:
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 4, 12, 5], 9));
+
+  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 7, 2, 15, 11, 14], 10));
+
+  console.log("");
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
